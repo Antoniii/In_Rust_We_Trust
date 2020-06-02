c1cecf09940d3614b4298be8cb3fae2ad8ca01fc
Addams <anto-kha1@mail.ru>
2020-06-02 18:27:35 +0400

    add file

---
+++ test/helloWorld.rs
@@ -0,0 +1,29 @@
+// curl https://sh.rustup.rs -sSf | sh
+/* source $HOME/.cargo/env
+rustc --version
+
+rustc 1.43.1 (8d69840ab 2020-05-04) */
+
+fn main() {
+	println!("Hello, мир!");
+}
+
+// source $HOME/.cargo/env
+// rustc helloWorld.rs
+// ./helloWorld
+
+
+/*
+cargo build
+./target/debug/hello_world
+*/
+
+// cargo run
+
+// cargo build --release # с оптимизацией
+
+
+/*
+скрипт удаления:
+sudo /usr/local/lib/rustlib/uninstall.sh
+*/
